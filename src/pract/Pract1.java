409c44910b311755a253e349ee5b88344a087bd3
nileshkharche92 <nileshkharche92@gmail.com>
2022-06-24 17:17:02 +0530

    d

---
+++ src/pract/Pract1.java
@@ -0,0 +1,14 @@
+package pract;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Pract1 {
+	public static void main(String[] args) {
+		
+	
+	System.setProperty("webdriver.chrome.driver","D:\\eclipse\\chromedriver.exe");
+WebDriver driver=new ChromeDriver();
+}
+	
+}
